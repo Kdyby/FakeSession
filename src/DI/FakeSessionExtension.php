68a99e59a13982c15f92b659fb7548fea893cdaa
Filip Procházka <filip@prochazka.su>
2015-01-18 20:40:32 +0100

    CompilerExtension

---
+++ src/DI/FakeSessionExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\FakeSession\DI;
+
+use Kdyby;
+use Nette;
+use Nette\PhpGenerator as Code;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class FakeSessionExtension extends Nette\DI\CompilerExtension
+{
+
+	/**
+	 * @var array
+	 */
+	public $defaults = array(
+		'enabled' => '%consoleMode%',
+	);
+
+
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$config = $this->getConfig($this->defaults);
+
+		if ($config['enabled']) {
+			$builder->getDefinition('session')
+				->setClass('Kdyby\FakeSession\Session');
+		}
+	}
+
+}
