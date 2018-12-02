2abfc4737348adaa944251d274b66fbc8cd0a909
Petr Morávek <petr@pada.cz>
2018-12-02 12:40:24 +0100

    tests: fix errors reported by PHPStan

--- tests/KdybyTests/bootstrap.php
+++ tests/KdybyTests/bootstrap.php
@@ -16,7 +16,6 @@ $loader->add('KdybyTests', __DIR__ . '/../');
 
 // configure environment
 Tester\Environment::setup();
-class_alias('Tester\Assert', 'Assert');
 date_default_timezone_set('Europe/Prague');
 
 // create temporary directory
@@ -28,11 +27,3 @@ $_SERVER = array_intersect_key($_SERVER, array_flip([
 	'PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS', 'argc', 'argv']));
 $_SERVER['REQUEST_TIME'] = 1234567890;
 $_ENV = $_GET = $_POST = [];
-
-function id($val) {
-	return $val;
-}
-
-function run(Tester\TestCase $testCase) {
-	$testCase->run();
-}
