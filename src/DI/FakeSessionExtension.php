251ee254a6378203600e819bf81492b581ab8406
Petr Morávek <petr@pada.cz>
2018-12-03 20:54:25 +0100

    Use strict types, add missing type hints and return types

--- src/DI/FakeSessionExtension.php
+++ src/DI/FakeSessionExtension.php
@@ -8,6 +8,8 @@
  * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
  */
 
+declare(strict_types = 1);
+
 namespace Kdyby\FakeSession\DI;
 
 use Kdyby;
@@ -26,7 +28,7 @@ class FakeSessionExtension extends \Nette\DI\CompilerExtension
 		'enabled' => '%consoleMode%',
 	];
 
-	public function beforeCompile()
+	public function beforeCompile(): void
 	{
 		$builder = $this->getContainerBuilder();
 		$config = $this->getConfig($this->defaults);
@@ -46,7 +48,7 @@ class FakeSessionExtension extends \Nette\DI\CompilerExtension
 		}
 	}
 
-	public static function register(Configurator $configurator)
+	public static function register(Configurator $configurator): void
 	{
 		$configurator->onCompile[] = function ($config, Compiler $compiler) {
 			$compiler->addExtension('fakeSession', new FakeSessionExtension());
